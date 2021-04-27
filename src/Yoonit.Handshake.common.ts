1a96d9321efb3e7261cd562461837e2214d63003
HaroldoTeruya <haroldo.s.teruya@gmail.com>
2021-04-27 15:13:46 -0300

    refactor: change name from SSL to handshake

---
+++ src/Yoonit.Handshake.common.ts
@@ -0,0 +1,18 @@
+// +-+-+-+-+-+-+
+// |y|o|o|n|i|t|
+// +-+-+-+-+-+-+
+//
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// | Yoonit Handshake Plugin for NativeScript applications           |
+// | Luigui Delyer, Gabriel Rizzo,                                   |
+// | Haroldo Teruya & Victor Goulart @ Cyberlabs AI 2020-2021        |
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+import {
+    Handshake as HandshakeDefinition,
+    ResultEventData
+} from '.';
+
+export abstract class HandshakeBase implements HandshakeDefinition {
+
+}
