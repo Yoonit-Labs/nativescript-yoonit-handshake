9988f043f578457931d272d971df0e323d0375e1
HaroldoTeruya <haroldo.s.teruya@gmail.com>
2021-04-27 15:16:22 -0300

    feat: add update fingerprint in the demo

--- demo-vue/app/components/App.vue
+++ demo-vue/app/components/App.vue
@@ -1,42 +1,53 @@
 <template>
   <Page @loaded="onLoaded">
+    <ActionBar title="Yoonit Camera"/>
+      <StackLayout>
+        <FlexboxLayout flexDirection="row">
+          <StackLayout orientation="horizontal">
+            <Button
+              text="Update Fingerprint"
+              horizontalAlignment="left"
+              @tap="updateFingerprint"
+            />
+          </StackLayout>
+        </FlexboxLayout>
+        <Label text="OEW" />
+      </StackLayout>
   </Page>
 </template>
 
 <script>
-import { CertStoreConfiguration, SecureDataStore } from '@yoonit/nativescript-handshake'
-
-  export default {
-    data: () => ({
-    }),
-
-    methods: {
-      async onLoaded() {
-        const certStoreConfig = new CertStoreConfiguration()
-        const secureStore = new SecureDataStore()
-
-        certStoreConfig.Builder({
-          url: 'https://www.cyberlabs.ai',
-          publicKey: 'cyberlabson'
-        })
-        .setUseChallenge(true)
-        .setIdentifier('teste')
-        .setPeriodicUpdateIntervalMillis(2000)
-        .setExpirationUpdateThresholdMillis(2000)
-
-        secureStore.save(certStoreConfig.getPublicKey(), 'cyberlabs key')
-
-        console.log('======= secure load ======', secureStore.load('cyberlabs key'))
-        console.log('[YooWultra] URL', certStoreConfig.getServiceUrl())
-        console.log('[YooWultra] useChallenge', certStoreConfig.getUseChallenge())
-        console.log('[YooWultra] getPeriodicUpdateIntervalMillis', certStoreConfig.getPeriodicUpdateIntervalMillis())
-        console.log('[YooWultra] getExpirationUpdateThresholdMillis', certStoreConfig.getExpirationUpdateThresholdMillis())
-        console.log('[YooWultra] publicKey', certStoreConfig.getPublicKey() === certStoreConfig.getPublicKey())
-        console.log('[YooWultra] Config build', certStoreConfig.getBuild())
-      }
+export default {
+  data: () => ({
+  }),
+  methods: {
+    async onLoaded() {
+      console.log(this.$yoo.handshake.updateFingerprints)
+    },
+    updateFingerprint() {
+      this.$yoo.handshake.updateFingerprints(
+          "BEcTK0YpgYom5ifxUyX5yKDNUBaaLQpWxU361n+ihb5EAnKvABWde1P6lnZR3CR9ykrTLrO0ANdivFabS20iGOA=",
+          "https://y9ggj3nnv8.execute-api.us-east-1.amazonaws.com/v1/sslpinning"
+      );
     }
-  }
+  },
+}
 </script>
 
 <style scoped>
+ActionBar {
+  background-color: #000000;
+  color: #ffffff;
+}
+Label {
+  font-size: 14;
+  color: #ffffff;
+}
+Button {
+  padding: 8 12;
+  color: #333333;
+  background-color: lightgray;
+  border-radius: 8;
+  margin: 8 0 8 12;
+}
 </style>
